94f5137857250ba71f1042286c6f784cb85b67f9
Shlok Gilda <shlok@ost.com>
2018-07-23 19:10:48 +0530

    Updated redis test env variables.

--- test/env/redis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export OST_DEFAULT_TTL='60'
-export OST_CACHING_ENGINE='redis'
-export OST_REDIS_HOST='localhost'
-export OST_REDIS_PORT='6380'
-export OST_REDIS_PASS='my-secret'
-export OST_REDIS_TLS_ENABLED='0'
