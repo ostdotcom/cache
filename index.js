eed29644245ecd3e202515624fcf468f625da158
Shlok Gilda <shlok@ost.com>
2018-07-25 16:20:22 +0530

    Updated logic of key generation for instanceMap

--- index.js
+++ index.js
@@ -2,26 +2,24 @@
  * Index File for openst-cache
  */
 
-"use strict";
+'use strict';
 
-const rootPrefix = "."
-  , version = require(rootPrefix + '/package.json').version
-  , OpenSTCacheKeys = require(rootPrefix + '/services/openst_cache_keys')
-  , InstanceComposer = require(rootPrefix + '/instance_composer')
-;
+const rootPrefix = '.',
+  version = require(rootPrefix + '/package.json').version,
+  OpenSTCacheKeys = require(rootPrefix + '/services/openst_cache_keys'),
+  InstanceComposer = require(rootPrefix + '/instance_composer');
 
 require(rootPrefix + '/services/cache_instance');
 
-const OpenSTCache = function (configStrategy) {
+const OpenSTCache = function(configStrategy) {
   const oThis = this;
 
   if (!configStrategy) {
-    throw "Mandatory argument configStrategy missing";
+    throw 'Mandatory argument configStrategy missing';
   }
 
-
   const instanceComposer = new InstanceComposer(configStrategy);
-  oThis.ic = function () {
+  oThis.ic = function() {
     return instanceComposer;
   };
 };
@@ -31,8 +29,8 @@ OpenSTCache.prototype = {
   OpenSTCacheKeys: OpenSTCacheKeys
 };
 
-Object.defineProperty(OpenSTCache.prototype, "cacheInstance", {
-  get: function () {
+Object.defineProperty(OpenSTCache.prototype, 'cacheInstance', {
+  get: function() {
     const oThis = this;
     return oThis.ic().getCacheInstance();
   }
@@ -46,15 +44,52 @@ const instanceMap = {};
  * @returns {string}
  *
  */
-const getInstanceKey = function (configStrategy) {
+const getInstanceKey = function(configStrategy) {
+  if (configStrategy.OST_CACHING_ENGINE == undefined) {
+    throw 'OST_CACHE_ENGINE parameter missing.';
+  }
 
-  // Fetch caching ID from the configStrategy.
-  return configStrategy.CACHING_STRATEGY_ID;
+  // Grab the required details from the configStrategy.
+  const cacheEngine = configStrategy.OST_CACHING_ENGINE.toString();
+  let isConsistentBehaviour = configStrategy.OST_CACHE_CONSISTENT_BEHAVIOR;
 
+  // Sanitize isConsistentBehaviour
+  isConsistentBehaviour = isConsistentBehaviour == undefined ? true : isConsistentBehaviour != '0';
+
+  // Stores the endpoint for key generation of instanceMap.
+  let endpointDetails = null;
+
+  // Generate endpointDetails for key generation of instanceMap.
+  if (cacheEngine == 'redis') {
+    const redisMandatoryParams = ['OST_REDIS_HOST', 'OST_REDIS_PORT', 'OST_REDIS_PASS', 'OST_REDIS_TLS_ENABLED'];
+
+    // Check if all the mandatory connection parameters for Redis are available or not.
+    for (let key = 0; key < redisMandatoryParams.length; key++) {
+      if (!configStrategy.hasOwnProperty(redisMandatoryParams[key])) {
+        throw 'Redis one or more mandatory connection parameters missing.';
+      }
+    }
+
+    endpointDetails =
+      configStrategy.OST_REDIS_HOST.toLowerCase() +
+      '-' +
+      configStrategy.OST_REDIS_PORT.toString() +
+      '-' +
+      configStrategy.OST_REDIS_TLS_ENABLED.toString();
+  } else if (cacheEngine == 'memcached') {
+    if (!configStrategy.hasOwnProperty('OST_MEMCACHE_SERVERS')) {
+      throw 'Memcached mandatory connection parameters missing.';
+    }
+
+    endpointDetails = configStrategy.OST_MEMCACHE_SERVERS.toLowerCase();
+  } else {
+    endpointDetails = '';
+  }
+
+  return cacheEngine + '-' + isConsistentBehaviour.toString() + '-' + endpointDetails;
 };
 
-
-let Factory = function () {};
+let Factory = function() {};
 
 Factory.prototype = {
   /**
@@ -64,7 +99,7 @@ Factory.prototype = {
    * @returns {cacheInstance}
    *
    */
-  getInstance: function (configStrategy) {
+  getInstance: function(configStrategy) {
     let instanceKey = getInstanceKey(configStrategy);
 
     let _instance = instanceMap[instanceKey];
@@ -79,7 +114,7 @@ Factory.prototype = {
 };
 
 const factory = new Factory();
-OpenSTCache.getInstance = function () {
+OpenSTCache.getInstance = function() {
   return factory.getInstance.apply(factory, arguments);
 };
 
